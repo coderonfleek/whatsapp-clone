cc13527dad487d74d1b9ac06c200fe3800f67193
Fikayo <fik4christ@yahoo.com>
2020-06-18 23:38:45 +0100

    Chat screen scaffold

--- src/pages/Tab1.jsx
+++ src/pages/Tab1.jsx
@@ -5,7 +5,10 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
-  useIonViewDidEnter
+  IonList,
+  IonItem,
+  IonAvatar,
+  IonLabel
 } from "@ionic/react";
 import ExploreContainer from "../components/ExploreContainer";
 import "./Tab1.css";
@@ -15,25 +18,23 @@ import { AppContext } from "../State";
 const Tab1 = () => {
   const { state, dispatch } = useContext(AppContext);
 
-  useIonViewDidEnter(async () => {
-    dispatch({
-      type: "loadUsers"
-    });
-  });
   return (
     <IonPage>
-      {/* <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader> */}
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      <IonContent className="chat-screen">
+        <IonList>
+          <IonItem>
+            <IonAvatar slot="start">
+              <img
+                src="https://media.istockphoto.com/photos/businessman-silhouette-as-avatar-or-default-profile-picture-picture-id476085198?k=6&m=476085198&s=612x612&w=0&h=5cDQxXHFzgyz8qYeBQu2gCZq1_TN0z40e_8ayzne0X0="
+                alt="icon"
+              />
+            </IonAvatar>
+            <IonLabel>
+              <h2>Han</h2>
+              <p>I've got enough on my plate as it is, and I...</p>
+            </IonLabel>
+          </IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   );
